2467b29cb98f3cf3768feca62e5e43dc52257bfc
Vishal Dwivedy <gautamdwivedy930@gmail.com>
2025-07-25 02:49:19 +0530

    "error fixed in route:user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getAllUsers } = require('../controllers/userController');
-const { protect, admin } = require('../middlewares/authMiddleware');
+const { protect, admin } = require('../middleware/authMiddleware');
 
 router.get('/', protect, admin, getAllUsers); // Admin-only route
 
